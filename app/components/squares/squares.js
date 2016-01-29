3b008bd9514f7b9d7e892252854f5c220dcee354
Eduardo Perez <eperez@kleermail.com>
2016-01-29 18:11:51 -0500

    user interaction

--- app/components/squares/squares.js
+++ app/components/squares/squares.js
@@ -5,7 +5,6 @@ squaresModule
       $scope.log = $log;
       $scope.squares = [];
       $scope.name="";
-      $scope.selected=0;
 
       for (var i=0; i<10; i++) {
         // inner loop applies to sub-arrays
@@ -22,14 +21,33 @@ squaresModule
 
 
       $scope.clickit = function(x,y) {
-        // foundationApi.publish('main-notifications', { title: 'Test', content: 'Test2' });
-        toastr.success("["+x+"]["+y+"]");
-        $scope.squares[x][y].selected = !$scope.squares[x][y].selected;
-        if ($scope.squares[x][y].selected) {
-          $scope.selected++;
+        if ($scope.name == '') {
+          toastr.error("Invalid Name");
+          return;
+        }
+
+        if ($scope.squares[x][y].selected){
+          $scope.squares[x][y].name = undefined;
+          $scope.squares[x][y].selected = false;
+
         }
         else {
-          $scope.selected--;
+          $scope.squares[x][y].selected = true;
+          $scope.squares[x][y].name = $scope.name;
         }
+      };
+
+      $scope.purchase = function() {
+        for (var i=0; i<10; i++) {
+          for (var j=0; j<10; j++) {
+            if ($scope.squares[i][j].selected) {
+              // set the name
+              $scope.squares[i][j].name = $scope.name;
+
+              // de-select it
+              $scope.clickit(i,j);
+            }
+          };
+        };
       };
     }]);
