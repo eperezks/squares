8818956f99e7a8f2b64c19a8f1079abcd0201934
Eduardo Perez <eperez@kleermail.com>
2016-01-30 09:01:24 -0500

    user interaction

--- app/components/squares/squares.js
+++ app/components/squares/squares.js
@@ -13,27 +13,68 @@ squaresModule
           // accesses each element of each sub-array in turn
           // console.log( $scope.squares[i][j] );
           $scope.squares[i].push({
-            selected: false
+            selected: false,
+            style: "square"
           });
         }
       }
 
+      $scope.isSelected = function(x,y) {
+        return $scope.squares[x][y].selected;
+      };
+      $scope.isMine = function(x,y) {
+        return ($scope.squares[x][y].name == $scope.name);
 
+      };
+
+      $scope.repaint = function() {
+        toastr.info('repaint');
+        for (var x=0; x<10; x++) {
+          for (var y=0; y<10; y++) {
+            $scope.setstyle(x,y);
+          }
+        }
+      }
 
       $scope.clickit = function(x,y) {
+
+        // need a name, dummy
         if ($scope.name == '') {
-          toastr.error("Invalid Name");
+          toastr.error("Name is required");
           return;
         }
 
-        if ($scope.squares[x][y].selected){
-          $scope.squares[x][y].name = undefined;
-          $scope.squares[x][y].selected = false;
-
+        // is this taken?
+        if ($scope.isSelected(x,y)){
+          // is it taken by the current user?
+          if ($scope.isMine(x,y)) {
+            $scope.squares[x][y].name = undefined;
+            $scope.squares[x][y].selected = false;
+          } else {
+            toastr.info('You do not own this bitch');
+          }
         }
         else {
           $scope.squares[x][y].selected = true;
           $scope.squares[x][y].name = $scope.name;
+        }
+        this.setstyle(x,y);
+      };
+
+      $scope.setstyle= function(x,y) {
+        // if it's owned by someone else then one color
+        if ($scope.isSelected(x,y)) {
+          if ($scope.isMine(x,y)) {
+            $scope.squares[x][y].style = "square sq-mine";
+          }
+          else {
+            // if it's owned by me then highlight it
+            $scope.squares[x][y].style = "square sq-selected";
+          }
+
+        }
+        else {
+          $scope.squares[x][y].style = "square";
         }
       };
 
